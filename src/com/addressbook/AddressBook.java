33e6ce723bd00dfc7956474b6ef0d6ae46aaf13d
Akshay444Kumar <aryakshay444@gmail.com>
2023-03-16 14:49:27 +0530

    Added a Welcome Message for Address Book Program

---
+++ src/com/addressbook/AddressBook.java
@@ -0,0 +1,7 @@
+package com.addressbook;
+
+public class AddressBook extends ContactDetails{
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+    }
+}
